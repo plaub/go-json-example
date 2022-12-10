c1c979e5bce997cf58cc74903e78889c53bf4339
plaub <pierrelaub@gmail.com>
2022-12-10 08:35:38 +0100

    refactor main readArguments

--- main.go
+++ main.go
@@ -11,20 +11,9 @@ import (
 )
 
 func main() {
-	if os.Args[1] == "help" {
-		fmt.Println("Usage: go run main.go [help|path] [path to data directory]")
+	path, shouldExit := readArguments()
+	if shouldExit {
 		return
-	}
-
-	var path string
-	if os.Args[1] == "path" {
-		path = os.Args[2]
-	} else {
-		panic("Please provide a path to the data directory.")
-	}
-
-	if (path[len(path)-1:]) != "/" {
-		path = path + "/"
 	}
 
 	files, filesError := readFilesFromPath(path)
@@ -40,6 +29,31 @@ func main() {
 	}
 
 	printSportsData(sportsDataArr, files)
+}
+
+// Read the arguments from the command line and return the path to the data directory.
+// If the user wants to see the help, the function returns true.
+// If the user does not provide a path, the function returns true.
+// If the path does not end with a slash, the function adds one.
+// If the user provides a path, the function returns the path and false.
+func readArguments() (path string, shouldExit bool) {
+	if os.Args[1] == "help" {
+		fmt.Println("Usage: go run main.go [help|path] [path to data directory]")
+		return "", true
+	}
+
+	if os.Args[1] == "path" {
+		path = os.Args[2]
+	} else {
+		fmt.Println("Please provide a path to the data directory.")
+		return "", true
+	}
+
+	if (path[len(path)-1:]) != "/" {
+		path = path + "/"
+	}
+
+	return path, false
 }
 
 func printSportsData(sportsDataArr []sports.SportsData, files []fs.FileInfo) {
